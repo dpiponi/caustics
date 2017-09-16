c805f20ff402d76b74fe074f3ef462783cd2ff10
dpiponi <dpiponi@gmail.com>
2017-09-16 15:05:44 -0700

    Update swallowtail.glsl

--- swallowtail.glsl
+++ swallowtail.glsl
@@ -31,15 +31,18 @@ vec2 cexp(vec2 a) {
 
 void mainImage( out vec4 fragColor, in vec2 fragCoord ) {
     const int N = 256;
-#define M 161
+    // Bands towards lower left and right are "aliasing" due to the sampling used
+    // in the integration. Nump up M to improve.
+    // M needs to be odd.
+#define M 197
     float rate = 0.005;
     
-    float xmin = -30.0;
-    float xmax = 30.0;
-    float ymin = -20.0;
-    float ymax = 50.0;
+    float xmin = -70.0;
+    float xmax = 70.0;
+    float ymin = -30.0;
+    float ymax = 110.0;
     
-    float gamma = -3.75+3.75*sin(iTime);
+    float gamma = -3.75+5.75*sin(iTime);
     
 	vec2 uv = fragCoord.xy / iResolution.xy;
     
@@ -51,6 +54,8 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord ) {
     
     float coeff;
     int odd = 1;
+    float x = -4.0;
+    float dx = 8.0/float(M);
     for (int k = 0; k <= M; ++k) {
         odd = 1-odd;
         // Simpson's integration rule
@@ -63,8 +68,12 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord ) {
                 coeff = 2.0;
             }
         }
-    	float x = -6.0+12.0*float(k)/float(M);
-    	float y = tanh(rate*(5.0*x*x*x*x+3.0*gamma*x*x+2.0*beta*x+alpha));
+        float x2 = x*x;
+        float x3 = x*x2;
+        float x4 = x*x3;
+        
+        float u = rate*(5.0*x4+3.0*gamma*x2+2.0*beta*x+alpha);
+    	float y = tanh(u);
         
         vec2 z = vec2(x, y);
         vec2 z2 = ctimes(z, z);
@@ -75,13 +84,17 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord ) {
         vec2 f = z5+gamma*z3+beta*z2+alpha*z;
 		vec2 g = cexp(vec2(-f.y, f.x));
        
-        float d = cosh(rate*(5.0*x*x*x*x+3.0*gamma*x*x+2.0*beta*x+alpha));
-        vec2 dz = vec2(1.0, rate*(20.0*x*x*x+6.0*gamma*x+2.0*beta)/(d*d));
+        float d = cosh(u);
+        vec2 dz = dx*vec2(1.0, rate*(20.0*x3+6.0*gamma*x+2.0*beta)/(d*d));
         
-        integral += coeff*g;
-    }
+
+ 		integral += coeff*ctimes(g, dz);
+
+        
+     	x += dx;
+   }
    
-    float value = 0.045*length(integral/3.0);
+    float value = 0.5*length(integral)/(3.0);
     
-    fragColor = vec4(value, value, value, 1.0);
+    fragColor = vec4(value);
 }
